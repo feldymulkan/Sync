8032b23991b3ade21ab64fa396894ab7d60aec45
Feldy Novanda Mulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2023-09-11 20:52:07 +0800

    Update ssh_manager.py

--- ssh_manager.py
+++ ssh_manager.py
@@ -19,5 +19,25 @@ class SSHManager:
         sftp.put(local_path, remote_path)
         sftp.close()
 
+    def create_folder(self, remote_path):
+        print(remote_path)
+        command = f'mkdir -p {remote_path}'
+        stdin, stdout, stderr = self.client.exec_command(command)
+        exit_status = stdout.channel.recv_exit_status()
+        if exit_status == 0:
+            print(f"Created folder on {self.hostname}:{remote_path}")
+        else:
+            error_message = stderr.read().decode()
+            print(f"Failed to create folder on {self.hostname}:{remote_path}: {error_message}")
+
     def close(self):
         self.client.close()
+
+# # Contoh penggunaan:
+# if __name__ == "__main__":
+#     ssh_manager = SSHManager('192.168.1.28', 22, 'osboxes', 'osboxes.org')
+#     remote_folder_path = '/home/osboxes/server2/'
+#     dirs = remote_folder_path.split('/')
+
+#     ssh_manager.create_folder(remote_folder_path+dirs[0])
+#     ssh_manager.close()
