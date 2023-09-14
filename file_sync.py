1cd7dc9849f8394fd8c1c119c6ba0362117e2f6d
Feldy Novanda Mulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2023-09-14 14:17:23 +0800

    Update file_sync.py

--- file_sync.py
+++ file_sync.py
@@ -3,10 +3,14 @@ import os
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from ssh_manager import SSHManager
+from LocalHost import LocalHost
+from Host import Host
+
 
 LOG = 'log.txt'
-# Folder lokal untuk sinkronisasi
-local_folder1 = "/home/kali/server1"
+local_folder = LocalHost.getLocalFolder
+# # Folder lokal untuk sinkronisasi
+# local_folder1 = "/home/kali/server1"
 class MyHandler(FileSystemEventHandler):
     def __init__(self, folder1, ssh_manager):
         self.folder1 = folder1
@@ -14,48 +18,50 @@ class MyHandler(FileSystemEventHandler):
         self.total_files_sent = 0
         self.total_bytes_sent = 0
 
-    def list_files_and_folders(directory):
-        files_and_folders = set()
-        for root, dirs, files in os.walk(directory):
-            for file in files:
-                files_and_folders.add(os.path.join(root, file))
-            for dir in dirs:
-                files_and_folders.add(os.path.join(root, dir))
-        return files_and_folders
+    # def list_files_and_folders(directory):
+    #     files_and_folders = set()
+    #     for root, dirs, files in os.walk(directory):
+    #         for file in files:
+    #             files_and_folders.add(os.path.join(root, file))
+    #         for dir in dirs:
+    #             files_and_folders.add(os.path.join(root, dir))
+    #     return files_and_folders
 
     def log(self, message):
         now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
         with open(LOG, 'a') as f:
             f.write('[' + now + ']' + message + '\n')
 
+    def folder_enum(self, direktori, remote_folder):
+        dirs = direktori.split('/')
+        newfolder_parts= []
+        start_adding = False
+        for dir_part in dirs:
+            if dir_part == os.path.basename(local_folder):
+                start_adding = True
+            elif start_adding:
+                newfolder_parts.append(dir_part)
+        new_folder_fix = os.path.join(remote_folder, *newfolder_parts)
+        return new_folder_fix
+        
     def on_created(self, event):
         remote_folder = "/home/osboxes/server2/"
         listFolder1 = os.listdir(self.folder1)
         if event.is_directory:
             print(f"Directory created: {event.src_path}")
             folder = event.src_path
-            dirs = folder.split('/')
-            newfolder_parts= []
-            start_adding = False
-            for dir_part in dirs:
-                if dir_part == os.path.basename(local_folder1):
-                    start_adding = True
-                elif start_adding:
-                    newfolder_parts.append(dir_part)
-            new_folder_fix = os.path.join(remote_folder, *newfolder_parts)
-            
-            ssh_manager.create_folder(new_folder_fix)
-            # ssh_manager.create_folder()
+            define_folder = self.folder_enum(folder, remote_folder)
+            ssh_manager.create_folder(define_folder)
         else:
             print(f"File created: {event.src_path}")
             for item in listFolder1:
                 folder1Item = os.path.join(self.folder1, item)
                 if os.path.isfile(folder1Item):
-                    start_time = time.time()
+                    # start_time = time.time()
                     self.log(f"Copying {item} to remote server")
                     remote_path = os.path.join(remote_folder, item)
                     self.ssh_manager.send_file(folder1Item, remote_path)
-                    end_time = time.time()
+                    # end_time = time.time()
 
                     # # Menghitung response time
                     # response_time = end_time - start_time
@@ -87,18 +93,27 @@ class MyHandler(FileSystemEventHandler):
 
                     # self.total_files_sent += 1
 
-# Informasi koneksi SSH
-hostname = "192.168.1.28"
+host_info = Host.read_host_info("hostname.txt")
+if host_info:
+    hostname = host_info.get("hostname", "")
+    port = int(host_info.get("port",))
+    username = host_info.get("username", "")
+    password = host_info.get("password", "")
+    print(hostname)
+    print(port)
+    print(username)
+    print(password)
+
+hostname = "192.168.103.190"
 port = 22
 username = "osboxes"
 password = "osboxes.org"
-
 # Inisialisasi SSHManager
 ssh_manager = SSHManager(hostname, port, username, password)
 
-event_handler = MyHandler(local_folder1, ssh_manager)
+event_handler = MyHandler(ssh_manager)
 observer = Observer()
-observer.schedule(event_handler, path=local_folder1, recursive=True)
+observer.schedule(event_handler, path=local_folder, recursive=True)
 observer.start()
 
 try:
@@ -113,5 +128,5 @@ observer.join()
 ssh_manager.close()
 
 # Menampilkan hasil pengukuran
-print("Total Files Sent:", event_handler.total_files_sent)
-print("Total Bytes Sent:", event_handler.total_bytes_sent)
+# print("Total Files Sent:", event_handler.total_files_sent)
+# print("Total Bytes Sent:", event_handler.total_bytes_sent)
