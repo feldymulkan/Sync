d09afc842ed5dd551f43dde2319177683bb3a001
Feldy Novanda Mulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2023-09-14 14:19:51 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,31 @@
+import socket
+import os
+import time
+from LocalHost import LocalHost
+import os
+
+
+def get_network_interfaces():
+# Mendapatkan daftar nama antarmuka jaringan
+    interfaces = os.listdir('/sys/class/net/')
+    return interfaces
+
+def main():
+    # Mendapatkan dan mencetak daftar nama antarmuka
+    interface_names = get_network_interfaces()
+    lhost = LocalHost("")
+    
+    print(f"Hostname : {lhost._hostname}")
+    for name in interface_names:
+        get_ip = lhost.getIp(name)
+        print(f"interfaces {name} : {get_ip}")
+    print("Welcome to file synhcronization")
+    local_direktori = input("Masukkan direktori anda : ")
+    lhost.localFolder = local_direktori
+    
+    
+    
+    
+
+if __name__ == "__main__":
+    main()
