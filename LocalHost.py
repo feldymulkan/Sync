f0a809b3021c03cfeb6fd5fc7db18d36b8450fd2
Feldy Novanda Mulkan <64622553+FeldyNovandaMulkan@users.noreply.github.com>
2023-09-14 14:19:23 +0800

    Create LocalHost.py

---
+++ LocalHost.py
@@ -0,0 +1,26 @@
+import os
+import socket
+from Host import Host
+import psutil
+
+class LocalHost(Host):
+    def __init__(self, localFolder):
+        self.hostname = socket.gethostname()
+        self.localFolder = localFolder
+    
+    def getLocalFolder(self):
+        return self.localFolder
+
+    def getIp(self, interface_name):
+        try:
+            addrs = psutil.net_if_addrs()
+            if interface_name in addrs:
+                ip_address = addrs[interface_name][0].address
+                return ip_address
+            else:
+                return None
+        except Exception as e:
+            print(f"Terjadi kesalahan: {e}")
+            return None
+
+
